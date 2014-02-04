bb822674dd0c20505ba1fad9772b47ab49d406c2
Hasse Feldthaus <hafe@kb.dk>
2014-02-04 18:56:06 +0100

    Refactor and code cleanup in gulpfile.js
    Removed changed (it didn't work?)
    Doing everything in one (default) go
    Renaming minified files as they are on existing system

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,41 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var changed = require('gulp-changed');
 var uglify = require('gulp-uglify');
 var cssmin = require('gulp-cssmin');
 var replace = require('gulp-replace');
+var rename = require('gulp-rename');
 
 var JSSRC = 'http-pub/*.js';
 var CSSSRC = 'http-pub/*.css';
 var HTMLSRC = 'http-pub/*.html';
 var DEST = 'production';
 var LOCALHOSTURL = 'http://localhost:8002/';
-var STATICURL = 'http://static.kb.dk/libcal/';
+var STATICURL = 'https://static.kb.dk/libcal/';
 
 gulp.task('default', function () {
-    return gulp.src(JSSRC)
-        .pipe(changed(DEST))
-        .pipe(uglify())
-        .pipe(gulp.dest(DEST));
-});
-
-gulp.task('minifyAndMoveJs', function (cb) {
-    gulp.src(JSSRC)
-    .pipe(uglify())
-    .pipe(replace(LOCALHOSTURL, STATICURL))
-    .pipe(gulp.dest(DEST));
-    cb();
-});
-
-gulp.task('minifyAndMoveCss', function (cb) {
-    gulp.src(CSSSRC)
-    .pipe(cssmin())
-    .pipe(replace(LOCALHOSTURL, STATICURL))
-    .pipe(gulp.dest(DEST));
-    cb();
-});
-
-gulp.task('moveHtml', function (cb) {
+    // move html files
     gulp.src(HTMLSRC)
     .pipe(replace(LOCALHOSTURL, STATICURL))
+    .pipe(replace('openingHours.js','openingHours_min.js')) // FIXME: This ought to be more generic, but gulp-replace does not work with regExp on streams??
     .pipe(gulp.dest(DEST));
-    cb();
-});
 
-gulp.task('prod', ['minifyAndMoveJs', 'minifyAndMoveCss', 'moveHtml']);
+    // minify and move js files
+    gulp.src(JSSRC)
+    .pipe(uglify())
+    .pipe(rename(function (dir, base, ext) {
+        return base + '_min' + ext;
+    }))
+    .pipe(replace('openingHoursStyles.css','openingHoursStyles_min.css')) // FIXME: This ought to be more generic, but gulp-replace does not work with regExp on streams??
+    .pipe(replace(LOCALHOSTURL, STATICURL))
+    .pipe(gulp.dest(DEST));
+
+    // minify and move css
+    gulp.src(CSSSRC)
+    .pipe(cssmin())
+    .pipe(rename(function (dir, base, ext) {
+        return base + '_min' + ext;
+    }))
+    .pipe(replace(LOCALHOSTURL, STATICURL))
+    .pipe(gulp.dest(DEST));
+});
 
